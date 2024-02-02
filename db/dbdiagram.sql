1793af74108c894c97c9b8981283f5721692ef6d
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-03 03:19:51 +0700

    Update bigint type, remove table Activities

--- db/dbdiagram.sql
+++ db/dbdiagram.sql
@@ -1,11 +1,11 @@
 /* dbdiagram.io Database Schema */
 
 Table Users {
-    id bigserial [pk] /* auto-increment */
+    id bigserial [pk] 
     username varchar [not null]
     email varchar [not null]
     full_name varchar [not null]
-    role int [not null, ref: > Roles.code]
+    role bigint [not null, ref: > Roles.code]
     status int [not null]
     password_hash varchar [not null]
     salt varchar [not null]
@@ -21,13 +21,14 @@ Table Users {
 }
 
 Table Projects {
-    id bigserial [pk] /* auto-increment */
+    id bigserial [pk] 
     name varchar [not null]
     description varchar
     status int [not null]
+    thumbnail_url varchar   
     priority int 
     privacy int 
-    created_by int [not null, ref: > Users.id]
+    created_by bigint [not null, ref: > Users.id]
     deadline timestamp  
     started_at timestamp
     created_at timestamp [default: `now()`]
@@ -36,11 +37,11 @@ Table Projects {
 }
 
 Table Tasks {
-    id bigserial [pk] /* auto-increment */
+    id bigserial [pk] 
     parent_task_id int 
-    project_id int [not null, ref: > Projects.id]
+    project_id bigint [not null, ref: > Projects.id]
     status int [not null]
-    created_by int [not null, ref: > Users.id]
+    created_by bigint [not null, ref: > Users.id]
     name varchar [not null]
     description varchar
     position float
@@ -54,10 +55,10 @@ Table Tasks {
 }
 
 Table Attachments {
-    id bigserial [pk] /* auto-increment */
+    id bigserial [pk] 
     task_id int [ref: > Tasks.id]
-    project_id int [ref: > Projects.id]
-    user_id int [ref: > Users.id]
+    project_id bigint [ref: > Projects.id]
+    created_by bigint [ref: > Users.id]
     file_name varchar
     file_path varchar
     created_at timestamp [default: `now()`]
@@ -65,7 +66,7 @@ Table Attachments {
 }
 
 Table Tags {
-    id bigserial [pk] /* auto-increment */
+    id bigserial [pk] 
     tag_type int /* 1: task, 2: project */
     name varchar    
     description varchar
@@ -73,59 +74,49 @@ Table Tags {
 }
 
 Table ProjectTags {
-    id bigserial [pk] /* auto-increment */
-    project_id int [ref: > Projects.id]
-    tag_id int [ref: > Tags.id]
+    id bigserial [pk] 
+    project_id bigint [ref: > Projects.id]
+    tag_id bigint [ref: > Tags.id]
     created_at timestamp [default: `now()`]
     updated_at timestamp [default: `now()`]
 }
 
 Table TaskTags {
-    id bigserial [pk] /* auto-increment */
-    task_id int [ref: > Tasks.id]
-    tag_id int [ref: > Tags.id]
+    id bigserial [pk] 
+    task_id bigint [ref: > Tasks.id]
+    tag_id bigint [ref: > Tags.id]
     created_at timestamp [default: `now()`]
     updated_at timestamp [default: `now()`]
 }
 
--- Table Activities {
---     id bigserial [pk] /* auto-increment */
---     project_id int [ref: > Projects.id]
---     task_id int [ref: > Tasks.id]
---     user_id int [ref: > Users.id]
---     content varchar
---     created_at timestamp [default: `now()`]
---     updated_at timestamp [default: `now()`]
--- }
-
 Table Notifications {
-    id bigserial [pk] /* auto-increment */
-    caused_by int [ref: > Users.id]
+    id bigserial [pk] 
+    caused_by bigint [ref: > Users.id]
     action_type varchar
     data json
     created_on timestamp [default: `now()`]
 }
 
 Table NotificationNotified {
-    id bigserial [pk] /* auto-increment */
-    notification_id int [ref: > Notifications.id]
-    user_id int [ref: > Users.id]
+    id bigserial [pk] 
+    notification_id bigint [ref: > Notifications.id]
+    user_id bigint [ref: > Users.id]
     read bool
     read_at timestamp [default: `now()`]
 }
 
 Table ProjectMembers {
-    id bigserial [pk] /* auto-increment */
-    project_id int [ref: > Projects.id]
-    user_id int [ref: > Users.id]
+    id bigserial [pk] 
+    project_id bigint [ref: > Projects.id]
+    user_id bigint [ref: > Users.id]
     added_at timestamp [default: `now()`]
     role_code varchar
 }
 
 Table ProjectMemberInvited {
-    id bigserial [pk] /* auto-increment */
-    project_id int [ref: > Projects.id]
-    user_account_invited_id int [ref: > Users.id]
+    id bigserial [pk] 
+    project_id bigint [ref: > Projects.id]
+    user_account_invited_id bigint [ref: > Users.id]
 }
 
 Table Roles {
@@ -133,32 +124,44 @@ Table Roles {
     name varchar
 }
 
-Table TaskActivities {
-    id bigserial [pk] /* auto-increment */
-    active bool
-    task_id int [ref: > Tasks.id]
+/*
+Table Activities {
+    id bigserial [pk] 
+    project_id bigint [ref: > Projects.id]
+    task_id bigint [ref: > Tasks.id]
+    user_id bigint [ref: > Users.id]
+    content varchar
     created_at timestamp [default: `now()`]
-    caused_by int [ref: > Users.id]
-    activity_type_id int
+    updated_at timestamp [default: `now()`]
+}
+*/
+
+Table TaskActivities {
+    id bigserial [pk] 
+    active bool
+    task_id bigint [ref: > Tasks.id]
+    created_at timestamp [default: `now()`]
+    caused_by bigint [ref: > Users.id] 
+    activity_type_id bigint [ref: > TaskActivityTypes.id]
     data json
 }
 
 Table TaskActivityTypes {
-    id bigserial [pk] /* auto-increment */
+    id bigserial [pk]
     code varchar
     template varchar
 }
 
 Table TaskAssigned {
-    id bigserial [pk] /* auto-increment */
-    task_id int [ref: > Tasks.id]
-    user_id int [ref: > Users.id]
+    id bigserial [pk] 
+    task_id bigint [ref: > Tasks.id]
+    user_id bigint [ref: > Users.id]
     assigned_date timestamp [default: `now()`]
 }
 
 Table TaskWatchers {
-    id bigserial [pk] /* auto-increment */
-    task_id int [ref: > Tasks.id]
-    user_id int [ref: > Users.id]
+    id bigserial [pk] 
+    task_id bigint [ref: > Tasks.id]
+    user_id bigint [ref: > Users.id]
     watched_at timestamp [default: `now()`]
 }
