c3a981c7ecc4375ed70e6d5308a84479da3e8f9a
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-22 10:05:26 +0700

    module(task): add model and storage

--- modules/task/storage/get_total_tasks.go
+++ modules/task/storage/get_total_tasks.go
@@ -1,15 +1,43 @@
 package storage
 
-//func (s *sqlStore) GetTotalTasks(ctx context.Context, cond map[string]interface{}) (*model.TodoItem, error) {
-//	var data *model.TodoItem
-//
-//	if err := s.db.Where(cond).First(&data).Error; err != nil {
-//		if err == gorm.ErrRecordNotFound {
-//			return nil, common.RecordNotFound
-//		}
-//
-//		return nil, common.ErrDB(err)
-//	}
-//
-//	return data, nil
-//}
+import (
+	"context"
+	"errors"
+	"gorm.io/gorm"
+	"iTask/common"
+	"iTask/modules/task/model"
+)
+
+func (s *sqlStore) GetTotalTasks(ctx context.Context, cond map[string]interface{}) (map[int]int, error) {
+	data := make(map[int]int)
+
+	// Select project_id, count(*) as total_tasks from Tasks group by project_id
+
+	type result struct {
+		ProjectID  int
+		TotalTasks int
+	}
+
+	var results []result
+
+	if err := s.db.Table(model.Task{}.TableName()).
+		//if err := s.db.Model(&model.Task{}).
+		Select("project_id, count(*) as total_tasks").
+		Where(cond).
+		Group("project_id").
+		//Pluck("project_id, total_tasks", &data).Error; err != nil {
+		Scan(&results).Error; err != nil {
+
+		if errors.Is(gorm.ErrRecordNotFound, err) {
+			return nil, common.RecordNotFound
+		}
+		return nil, common.ErrorDB(err)
+
+	}
+
+	for _, result := range results {
+		data[result.ProjectID] = result.TotalTasks
+	}
+
+	return data, nil
+}
