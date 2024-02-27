070f4a2530bfa8e8e0885d2bb020667e4e9c9170
trankhanh040147 <trankhanh040147@gmail.com>
2024-02-27 15:52:08 +0700

    add: update project by id

--- modules/project/model/project-update.go
+++ modules/project/model/project-update.go
@@ -1,10 +1,16 @@
 package model
 
+import "time"
+
 type ProjectUpdate struct {
 	// use pointer(*) to allow update data to "", 0, false... except nil
-	Title       *string `json:"title" gorm:"column:title;"`
-	Description *string `json:"description" gorm:"column:;description"`
-	Status      *string `json:"status" gorm:"column:status;"`
+	Name        *string    `json:"name" gorm:"column:name"`
+	Description *string    `json:"description" gorm:"column:description"`
+	Status      *int       `json:"status" gorm:"column:status"`
+	Thumbnail   *string    `json:"thumbnail_url" gorm:"column:thumbnail_url"`
+	Privacy     *string    `json:"privacy" gorm:"column:privacy"`
+	Deadline    *string    `json:"deadline" gorm:"column:deadline"`
+	StartedAt   *time.Time `json:"started_at" gorm:"column:started_at"`
 }
 
 func (ProjectUpdate) TableName() string { return Project{}.TableName() }
