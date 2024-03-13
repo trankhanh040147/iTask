8fdf785d7b78e089c9233312f0e627f518d85612
trankhanh040147 <trankhanh040147@gmail.com>
2024-03-13 22:54:54 +0700

    add filter tag_type for |GET /tags|

--- modules/tag/storage/list.go
+++ modules/tag/storage/list.go
@@ -4,12 +4,24 @@ import (
 	"context"
 	"iTask/common"
 	"iTask/modules/tag/model"
+	"strings"
 )
 
 func (s *sqlStore) ListTag(ctx context.Context, filter *model.Filter, paging *common.Paging, moreKeys ...string) ([]model.Tag, error) {
 	var result []model.Tag
 
 	db := s.db.Table(model.Tag{}.TableName())
+
+	if filter != nil {
+		filter.TagType = strings.ToLower(filter.TagType)
+
+		if filter.TagType == "project" {
+			db = db.Where("tag_type = ?", model.TypeProject)
+		}
+		if filter.TagType == "task" {
+			db = db.Where("tag_type = ?", model.TypeTask)
+		}
+	}
 
 	if err := db.Select("id").Count(&paging.Total).Error; err != nil {
 		return nil, common.ErrDB(err)
