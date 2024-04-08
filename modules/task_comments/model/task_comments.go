98b8554536c942e822ef8d65cb651555a0fd8cfd
Tran Khanh <trankhanh040147@gmail.com>
2024-04-08 10:10:52 +0700

    create module task_comments

--- modules/task_comments/model/task_comments.go
+++ modules/task_comments/model/task_comments.go
@@ -1,1 +1,28 @@
 package model
+
+import (
+	"errors"
+	"iTask/common"
+)
+
+var (
+	ErrCreatedByCannotBeEmpty = errors.New("Created by cannot be empty")
+	ErrMessageCannotBeEmpty   = errors.New("Message cannot be empty")
+)
+
+const (
+	EntityName = "TaskComments"
+)
+
+type TaskComments struct {
+	common.SQLModel
+	TaskId          int    `json:"task_id" gorm:"column:task_id"`
+	CreatedBy       int    `json:"created_by" gorm:"column:created_by"`
+	ParentCommentId int    `json:"parent_comment_id" gorm:"column:parent_comment_id"`
+	IsPinned        bool   `json:"is_pinned" gorm:"column:pinned"`
+	Message         string `json:"message" gorm:"column:message"`
+}
+
+func (TaskComments) TableName() string { return "TaskComments" }
+
+func (a *TaskComments) GetCreatedBy() int { return a.CreatedBy }
